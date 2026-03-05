2f219e5f64f732224b9e69c5f7983b890e407e59
Kael <kaelwd@gmail.com>
2026-03-05 18:16:12 +1100

    fix: actually run form-slot-refs and grid plugins

--- src/plugins/v4/index.ts
+++ src/plugins/v4/index.ts
@@ -1,6 +1,8 @@
 import type { Plugin } from 'vue-metamorph'
 import { v4ComboboxItemSlotPlugin } from './combobox-item-slot'
 import { v4ElevationPlugin } from './elevation'
+import { v4FormSlotRefsPlugin } from './form-slot-refs'
+import { v4GridPlugin } from './grid'
 import { v4SnackbarMultilinePlugin } from './snackbar-multiline'
 import { v4SnackbarQueueSlotPlugin } from './snackbar-queue-slot'
 import { v4TypographyPlugin } from './typography'
@@ -9,6 +11,8 @@ export function vuetify4 (): Plugin[] {
   return [
     v4ComboboxItemSlotPlugin,
     v4ElevationPlugin,
+    v4FormSlotRefsPlugin,
+    v4GridPlugin,
     v4SnackbarMultilinePlugin,
     v4SnackbarQueueSlotPlugin,
     v4TypographyPlugin,
